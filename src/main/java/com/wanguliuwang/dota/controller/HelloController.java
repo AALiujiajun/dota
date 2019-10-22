86119a12658742afc41d3371ab0575ba4466bf76
soyuchong <317091034@qq.com>
2019-10-22 16:57:41 +0800

    init repo

---
+++ src/main/java/com/wanguliuwang/dota/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.wanguliuwang.dota.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    public String hello(@RequestParam(name = "name")String name, Model model)
+    {
+        return "hello";
+    }
+}
